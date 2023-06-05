4e9474821c21a8710f1e09f189cd7d5d5e792621
alexmrin <alex.matsumura@akane.waseda.jp>
2023-06-05 20:16:56 +0900

    checker jpg

--- FeatureExtraction/MainFeature.py
+++ FeatureExtraction/MainFeature.py
@@ -3,15 +3,16 @@ import numpy as np
 from FASTCorner import fast_corners 
 from HarrisCorner import harris_corners
 
-imagepath = "/Users/alexa/vSLAM/testimages/fasttestimage1.jpg"
+imagepath = "/Users/alexa/vSLAM/testimages/checkers.jpg"
 testimage = cv2.imread(imagepath)
 threshold = 10
 pixelnumbers = 12
 #conversion to grayscale
 grayframe = cv2.cvtColor(testimage, cv2.COLOR_BGR2GRAY)
 fastcorners = fast_corners(grayframe, threshold, pixelnumbers)
+print(fastcorners)
 fastcorners = harris_corners(grayframe, fastcorners, 0.04, 500)
-# print(fastcorners)
+print(fastcorners)
 
 for corner in fastcorners:
     cv2.circle(testimage, corner, radius = 3, color = (0,255,0), thickness = 1)
