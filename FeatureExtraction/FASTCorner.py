8142bb0cd398cc05fdd467b4ffec90142ddfeb64
TI <131604147+ti-556@users.noreply.github.com>
2023-06-06 22:08:01 +0900

    Update FASTCorner.py
    Better processing time

--- FeatureExtraction/FASTCorner.py
+++ FeatureExtraction/FASTCorner.py
@@ -13,34 +13,35 @@ def fast_corners(grayframe, threshold, diffpixelnum):
         for j in range(3, width - 3):
             pixelpos = grayframe[i, j]
             
-            #circle patttern
-            patterns = [grayframe[i - 3, j - 1], grayframe[i - 3, j], grayframe[i - 3, j + 1],
-                        grayframe[i - 2, j + 1], grayframe[i - 1, j + 2], grayframe[i, j + 3],
-                        grayframe[i + 1, j + 2], grayframe[i + 2, j + 1], grayframe[i + 3, j],
-                        grayframe[i + 2, j - 1], grayframe[i + 1, j - 2], grayframe[i, j - 3],
-                        grayframe[i - 1, j - 2], grayframe[i - 2, j - 1]]
-            
             #a pattern of pixels 1, 5, 9, 13
             fastpatterns = [grayframe[i + 3, j], grayframe[i, j + 3], grayframe[i - 3, j], grayframe[i, j - 3]]
+            
+            #Bresenham circle patttern
+            patterns = [grayframe[i - 3, j - 1], grayframe[i - 3, j], grayframe[i - 3, j + 1],
+            grayframe[i - 2, j + 2], grayframe[i - 1, j + 3], grayframe[i, j + 3],
+            grayframe[i + 1, j + 3], grayframe[i + 2, j + 2], grayframe[i + 3, j + 1],
+            grayframe[i + 3, j], grayframe[i + 3, j - 1], grayframe[i + 2, j - 2],
+            grayframe[i + 1, j - 3], grayframe[i, j - 3], grayframe[i - 1, j - 3],
+            grayframe[i - 2, j - 2]]
             
             #Ip + t
             overthresholdvalue = pixelpos + threshold
             #Ip - t
             underthresholdvalue = pixelpos - threshold
             
-            #number of pixels in the circle that are overthreshold or underthreshold
-            overthreshold = np.sum(np.array(patterns) > overthresholdvalue)
-            underthreshold = np.sum(np.array(patterns) < underthresholdvalue)
             #number of pixels from 1, 5, 9, 13 that are overthreshold or underthreshold
             fastoverthreshold = np.sum(np.array(fastpatterns) > overthresholdvalue)
             fastunderthreshold = np.sum(np.array(fastpatterns) < underthresholdvalue)
             
             #there must be at least 3 pixels that are overthreshold or underthreshold from pixels 1, 5, 9, 13 to be a corner  
             if fastoverthreshold >= 3 or fastunderthreshold >=3:
+                
+                #number of pixels in the circle that are overthreshold or underthreshold
+                overthreshold = np.sum(np.array(patterns) > overthresholdvalue)
+                underthreshold = np.sum(np.array(patterns) < underthresholdvalue)
+                
                 #a pixel is a corner if there are n or more pixels in the circle that are overthreshold or underthreshold
                 if overthreshold >= diffpixelnum or underthreshold >= diffpixelnum:
                     corners.append((j, i))
     
     return corners
-
-
